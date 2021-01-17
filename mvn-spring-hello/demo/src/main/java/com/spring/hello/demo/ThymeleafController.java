4fca95d9e03da435a525bc966c3a11210667a027
basileu <basil.boli@yahoo.com>
2021-01-17 18:41:37 +0200

    Adding support for Model class leverage in the controller. Modifying html to intercept Model params by Thymeleaf
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- mvn-spring-hello/demo/src/main/java/com/spring/hello/demo/ThymeleafController.java
+++ mvn-spring-hello/demo/src/main/java/com/spring/hello/demo/ThymeleafController.java
@@ -1,14 +1,31 @@
 package com.spring.hello.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ThymeleafController {
 
+    private List<String> list;
+    
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("paramName0", "Hei Mambo!");
+        model.addAttribute("paramName1", "Mambo italiano!");
+
+        model.addAttribute("list", list);
+
         return "index";
+    }
+
+    public ThymeleafController() {
+        this.list = new ArrayList<>();
+        list.add("Hello world");
+        this.list.add("+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.");
     }
 }
